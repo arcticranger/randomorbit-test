655fe0a4d58b2b8d6bde648a928873a51afed2bd
arcticranger <kurtsteele@yahoo.com>
2013-08-27 14:56:51 -0400

    new user domain and enum classes

--- grails-app/domain/randomorbit/test/User.groovy
+++ grails-app/domain/randomorbit/test/User.groovy
@@ -2,6 +2,24 @@ package randomorbit.test
 
 class User {
 
+    String userId
+    String password
+    String email
+
+    Date dateCreated
+
+    EthnicEnum ethnic
+    HairEnum hair
+    BuildEnum build
+
+
+    //static hasMany = [ posts : Post, tags : Tag, following : User ]
+
+
     static constraints = {
+        userId(blank:false, unique: true)
+        password(blank:false)
+        email(email: true, nullable: true)
+        dateCreated()
     }
 }
